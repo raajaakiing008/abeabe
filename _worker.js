12ae27a9a69eb1a7fb12ac8cf5b61a3e043bfaf3
raajaakiing008 <raajaakiing.008@gmail.com>
2025-09-16 16:58:16 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 import { connect } from "cloudflare:sockets";
 
 // Variables
-let serviceName = "";
-let APP_DOMAIN = "";
+let serviceName = "abeabe";
+let APP_DOMAIN = "raajaakiing-008.workers.dev";
 
 let prxIP = "";
 let cachedPrxList = [];
@@ -15,7 +15,7 @@ const neko = "Y2xhc2g=";
 
 const PORTS = [443, 80];
 const PROTOCOLS = [atob(horse), atob(flash), "ss"];
-const SUB_PAGE_URL = "https://abeabe.raajaakiing-008.workers.dev/sub";
+const SUB_PAGE_URL = "https://foolvpn.me/nautica";
 const KV_PRX_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/kvProxyList.json";
 const PRX_BANK_URL = "https://raw.githubusercontent.com/FoolVPN-ID/Nautica/refs/heads/main/proxyList.txt";
 const DNS_SERVER_ADDRESS = "8.8.8.8";
